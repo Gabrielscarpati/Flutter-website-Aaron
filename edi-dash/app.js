c0a5dba0a689c336fb4db9bf8f0e5bd711806d35
allannmsilva <allannevesmsilva@gmail.com>
2024-05-09 10:44:42 -0300

    init backend

---
+++ edi-dash/app.js
@@ -0,0 +1,70 @@
+require('dotenv').config();
+
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+var passport = require('passport');
+// const LocalStrategy = require('passport-local').Strategy;
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    port: process.env.DB_PORT,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json({
+    extended: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configurar o express-session
+app.use(session({
+    secret: 'secreto', // Chave secreta para assinar a sessão
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Middleware de inicialização do passport
+app.use(passport.initialize());
+
+// Middleware de persistência de sessão
+app.use(passport.session());
+
+// Middleware para adicionar erros de autenticação e o usuário atual ao objeto res.locals
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+
+app.get('/', async (_, res) => {
+    try {
+        connection.query('SELECT * FROM seller', function (error, results, _fields) {
+            if (error) {
+                res.json({
+                    error
+                });
+            } else {
+                res.json(results);
+            }
+        });
+    } catch (error) {
+        res.json({
+            error
+        });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}/`);
+});
