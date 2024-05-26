9454e59bc1a0159cdc3210e04a27ef8672ebcc99
Geanderson <geandersonlemonte.gl@gmail.com>
2024-05-26 15:29:45 -0300

    fix caddy server

--- frontend/lib/app/connection/api_connection.dart
+++ frontend/lib/app/connection/api_connection.dart
@@ -5,7 +5,7 @@ import 'auth_interceptor.dart';
 
 class ApiConnection {
   String getApiUrl() {
-    return "http://stage.evedi.io/api/";
+    return "https://stage.evedi.io/api/";
   }
 
   late final String _url = getApiUrl();
